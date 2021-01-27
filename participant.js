1c60a3917dd8cf73664874f5911de23158c3ca95
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2021-01-27 23:41:27 +0100

    Added debug code.

--- participant.js
+++ participant.js
@@ -1,3 +1,60 @@
-onmessage = messageEvent => {
-	postMessage(/*Response*/);
+'use strict'
+let right = 1;
+let team;
+let settings;
+let opponents;
+ParticipantHelper.init = data => {
+	team = data.opponents.findIndex(opponent=>opponent===null);
+	settings = data.settings;
+	opponents = data.opponents;
+	console.log(settings);
+	console.log(opponents);
+}
+ParticipantHelper.onmessage = data => {
+	if(right){
+		let max = data.message.length-1;
+		let mid = max/2;
+		let teamForward;
+		let teamLeft;
+		let teamRight;
+		switch(team){
+			case 0:
+				teamForward = 1;
+				teamLeft = 2;
+				teamRight = 3;
+				break;
+			case 1:
+				teamForward = 0;
+				teamLeft = 3;
+				teamRight = 2;
+				break;
+			case 2:
+				teamForward = 3;
+				teamLeft = 1;
+				teamRight = 0;
+				break;
+			case 3:
+				teamForward = 2;
+				teamLeft = 0;
+				teamRight = 1;
+				break;
+		}
+		let teamCorrect = team === data.message[mid][0].occupiedBy.team;
+		let teamForwardCorrect = teamForward === data.message[mid][max].occupiedBy.team;
+		let teamLeftCorrect = true;
+		let teamRightCorrect = true;
+		if(2 < opponents.length){
+			teamLeftCorrect = teamLeft === data.message[0][mid].occupiedBy.team;
+			teamRightCorrect = teamRight === data.message[max][mid].occupiedBy.team;
+		}
+		if(!teamCorrect || !teamForwardCorrect || !teamLeftCorrect || !teamRightCorrect){
+			console.log('team('+team+'): '+teamCorrect+'\n'+
+			'teamForward: '+teamForward+', '+teamForwardCorrect+'\n'+
+			'teamLeft: '+teamLeft + ', ' +teamLeftCorrect+'\n'+
+			'teamRight: '+teamRight + ', ' +teamRightCorrect);
+			debugger;
+		}
+	}
+	ParticipantHelper.respond(right);
+	right = 0;
 }
