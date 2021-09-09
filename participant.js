f501c4a06e623e1d0511d4f0208d3a201cd67902
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2021-09-09 22:55:36 +0200

    Optimized and fixed arena orientation.

--- participant.js
+++ participant.js
@@ -3,7 +3,6 @@ let _right = 1;
 let _team;
 let _settings;
 let _opponents;
-let _pos;
 let _tick = 0;
 let _responses = {
 	FORWARD: 'y+',
@@ -33,50 +32,52 @@ function isSpaceOpen(data, x, y, z){
 	}
 	return false;
 }
-ParticipantHelper.onmessage = message => {
-//	ParticipantHelper.onmessage = message => {
-//		message.respond(0);
-//	}
-//	message.respond(1);
-//	return;
-	// TEMP ^^^^
-	let data = message.data;
-	if(_tick === 0){
-		let max = data[0].length-1;
-		let mid = max/2;
-		let mid_z = (data.length-1)/2;
-		_pos = {
-			x: mid,
-			y: 0,
-			z: mid_z
-		};
+function getPos(data){
+	for(const z in data){
+		const height = data[z];
+		for(const x in height){
+			const column = height[x];
+			for(const y in column){
+				const space = column[y];
+				if(space.occupiedBy !== null && space.occupiedBy.team === _team && space.occupiedBy.type === 'SolidWorm'){
+					return {x: parseInt(x), y: parseInt(y), z: parseInt(z)};
+				}
+			}
+		}
 	}
+}
+function getPossibleResponses(data, pos){
 	let possibleResponses = [];
-	if(_currentDirection !== _responses.BACKWARDS && isSpaceOpen(data, _pos.x, _pos.y+1, _pos.z)){
+	if(_currentDirection !== _responses.BACKWARDS && isSpaceOpen(data, pos.x, pos.y+1, pos.z)){
 		possibleResponses.push(_responses.FORWARD);
 	}
-	if(_currentDirection !== _responses.FORWARD && isSpaceOpen(data, _pos.x, _pos.y-1, _pos.z)){
+	if(_currentDirection !== _responses.FORWARD && isSpaceOpen(data, pos.x, pos.y-1, pos.z)){
 		possibleResponses.push(_responses.BACKWARDS);
 	}
-	if(_currentDirection !== _responses.LEFT && isSpaceOpen(data, _pos.x+1, _pos.y, _pos.z)){
+	if(_currentDirection !== _responses.LEFT && isSpaceOpen(data, pos.x+1, pos.y, pos.z)){
 		possibleResponses.push(_responses.RIGHT);
 	}
-	if(_currentDirection !== _responses.RIGHT && isSpaceOpen(data, _pos.x-1, _pos.y, _pos.z)){
+	if(_currentDirection !== _responses.RIGHT && isSpaceOpen(data, pos.x-1, pos.y, pos.z)){
 		possibleResponses.push(_responses.LEFT);
 	}
-	if(_currentDirection !== _responses.DOWN && isSpaceOpen(data, _pos.x, _pos.y, _pos.z+1)){
-		possibleResponses.push(_responses.UP);
+	if(_currentDirection !== _responses.DOWN && isSpaceOpen(data, pos.x, pos.y, pos.z+1)){
+//		possibleResponses.push(_responses.UP);
 	}
-	if(_currentDirection !== _responses.UP && isSpaceOpen(data, _pos.x, _pos.y, _pos.z-1)){
-		possibleResponses.push(_responses.DOWN);
+	if(_currentDirection !== _responses.UP && isSpaceOpen(data, pos.x, pos.y, pos.z-1)){
+//		possibleResponses.push(_responses.DOWN);
 	}
+	return possibleResponses;
+}
+ParticipantHelper.onmessage = message => {
+	let data = message.data;
+	let pos = getPos(data);
 	if(!_target || !data[_target.pos_z][_target.pos_x][_target.pos_y].eatables.apple){
 		let eatables = [];
 		data.forEach((height, z) => {
 			height.forEach((column, x) => {
 				column.forEach((space, y) => {
 					if(space.eatables.apple || 0 < space.eatables.other){
-						let e = {distance: Math.abs(_pos.x-x)+Math.abs(_pos.y-y)+Math.abs(_pos.z-z), eatables: space.eatables.other, pos_z: z, pos_x: x, pos_y: y};
+						let e = {distance: Math.abs(pos.x-x)+Math.abs(pos.y-y)+Math.abs(pos.z-z), eatables: space.eatables.other, pos_z: z, pos_x: x, pos_y: y};
 						if(space.eatables.apple){
 							e.eatables++;
 						}
@@ -87,85 +88,32 @@ ParticipantHelper.onmessage = message => {
 		});
 		_target = eatables[Math.floor(Math.random()*eatables.length)];
 	}
+	let dirOptions = [];
 	if(_target){
-		let dx = _target.pos_x-_pos.x;
-		let dy = _target.pos_y-_pos.y;
-		let dz = _target.pos_z-_pos.z;
-		if(_team===0){debugger}
-		if(dx !== 0 && Math.abs(dy) < Math.abs(dx)){
-			if(0 < dx){
-				switch(_currentDirection){
-					case _responses.FORWARD:
-						_currentDirection = _responses.RIGHT;
-						break;
-					case _responses.BACKWARDS:
-						_currentDirection = _responses.LEFT;
-						break;
-					case _responses.LEFT:
-						_currentDirection = Math.round(Math.random()) === 0 ? _responses.LEFT : _responses.RIGHT;
-						break;
-					default:
-						break;
-				}
-			}else{
-				switch(_currentDirection){
-					case _responses.FORWARD:
-						_currentDirection = _responses.LEFT;
-						break;
-					case _responses.BACKWARDS:
-						_currentDirection = _responses.RIGHT;
-						break;
-					case _responses.RIGHT:
-						_currentDirection = Math.round(Math.random()) === 0 ? _responses.LEFT : _responses.RIGHT;
-						break;
-					default:
-						break;
-				}
-			}
-		}else{
-			if(0 < dy){
-				switch(_currentDirection){
-					case _responses.BACKWARDS:
-						_currentDirection = Math.round(Math.random()) === 0 ? _responses.LEFT : _responses.RIGHT;
-						break;
-					case _responses.LEFT:
-						_currentDirection = _responses.RIGHT;
-						break;
-					case _responses.RIGHT:
-						_currentDirection = _responses.LEFT;
-						break;
-					default:
-						break;
-				}
-			}else{
-				switch(_currentDirection){
-					case _responses.FORWARD:
-						_currentDirection = Math.round(Math.random()) === 0 ? _responses.LEFT : _responses.RIGHT;
-						break;
-					case _responses.LEFT:
-						_currentDirection = _responses.LEFT;
-						break;
-					case _responses.RIGHT:
-						_currentDirection = _responses.RIGHT;
-						break;
-					default:
-						break;
-				}
+		let dx = _target.pos_x-pos.x;
+		let dy = _target.pos_y-pos.y;
+		let dz = _target.pos_z-pos.z;
+		dirOptions.push({dir: _responses.FORWARD, nextDistance: Math.abs(dx)+Math.abs(dy-1)+Math.abs(dz)});
+		dirOptions.push({dir: _responses.BACKWARDS, nextDistance: Math.abs(dx)+Math.abs(dy+1)+Math.abs(dz)});
+		dirOptions.push({dir: _responses.RIGHT, nextDistance: Math.abs(dx-1)+Math.abs(dy)+Math.abs(dz)});
+		dirOptions.push({dir: _responses.LEFT, nextDistance: Math.abs(dx+1)+Math.abs(dy)+Math.abs(dz)});
+		dirOptions.forEach(option => option.sort = option.nextDistance + Math.random()); // Shuffle equal distance.
+		let possibleResponses = getPossibleResponses(data, pos);
+		for(const option of dirOptions.sort((o1, o2) => o1.sort - o2.sort)){
+			if(possibleResponses.includes(option.dir)){
+				_currentDirection = option.dir;
+				break
 			}
 		}
 	}
-	if(!possibleResponses.includes(_currentDirection) && 0 < possibleResponses.length){
-		_currentDirection = possibleResponses[Math.floor(Math.random()*possibleResponses.length)];
-	}
 	message.respond(_currentDirection);
-	if(_team===2){console.log(_currentDirection)}
 	switch(_currentDirection){
-		case _responses.FORWARD: _pos.y++; break;
-		case _responses.BACKWARDS: _pos.y--; break;
-		case _responses.LEFT: _pos.x--; break;
-		case _responses.RIGHT: _pos.x++; break;
-		case _responses.UP: _pos.z++; break;
-		case _responses.DOWN: _pos.z--; break;
+		case _responses.FORWARD: pos.y++; break;
+		case _responses.BACKWARDS: pos.y--; break;
+		case _responses.LEFT: pos.x--; break;
+		case _responses.RIGHT: pos.x++; break;
+		case _responses.UP: pos.z++; break;
+		case _responses.DOWN: pos.z--; break;
 	}
 	_tick++;
 }
